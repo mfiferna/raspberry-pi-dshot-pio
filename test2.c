8af22370b1fb4e454c350c186182b60d9dd28fd1
Marian Vittek <vittek@xrefactory.com>
2025-01-21 10:33:54 +0100

    dma and 3d mode

--- test2.c
+++ test2.c
@@ -2,61 +2,93 @@
   This example rotates 4 motors, each of them in normal and then reversed direction.
  */
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 extern void motorImplementationInitialize(int motorPins[], int motorMax) ;
 extern void motorImplementationFinalize(int motorPins[], int motorMax) ;
 extern void motorImplementationSendThrottles(int motorPins[], int motorMax, double motorThrottle[]) ;
 extern void motorImplementationSet3dModeAndSpinDirection(int motorPins[], int motorMax, int mode3dFlag, int reverseDirectionFlag) ;
+extern void dshotRepeatSendCommand(int motorPins[], int motorMax, int cmd, int telemetry, int repeatCounter) ;
 
-// let's say we have 4 motors connected to GPIO6, GPIO13, GPIO19 and GPIO26
-#define N 4
-//int motorPins[N] = {6, 13, 19, 26};
-int motorPins[N] = {16, 13, 19, 26};
+int 	motorPins[32];
+double 	throttles[32];
 
-double throttles[N];
+int main(int argc, char **argv) {
+    int i, n, k;
 
-int main() {
-    int i, k;
+    if (argc < 2) {
+	printf("usage:   ./test2 <gpio_pin_1> ... <gpio_pin_n>\n");
+	printf("example: sudo ./test2 16 19 20 21\n");
+	exit(1);
+    }
 
-    motorImplementationInitialize(motorPins, N);
-    motorImplementationSet3dModeAndSpinDirection(motorPins, N, 1, 0);
+    n = 0;
+    for(i=1; i<argc; i++) {
+	motorPins[n] = atoi(argv[i]);
+	if (motorPins[n] > 0 && motorPins[n] < 28) {
+	    n ++;
+	} else {
+	    printf("pin %d out of range 1..27\n", motorPins[n]);
+	}
+    }
+    
+    motorImplementationInitialize(motorPins, n);
+    
+    printf("Setting 3d mode and rotation direction. It may take a few seconds.\n");
+    motorImplementationSet3dModeAndSpinDirection(motorPins, n, 1, 0);
 
     // make spinning 1 motor after another
-    for(i=0; i<N; i++) throttles[i] = 0;
-    for(k=0;k<12;k++) {
+    for(k=0;k<n;k++) {
 	// spin
-	throttles[k%4] = 0.1;
-	for(i=0; i<100; i++) {
-	    motorImplementationSendThrottles(motorPins, N, throttles);
-	    usleep(10000);
+	printf("Spinning motor %d.\n", k%n);
+	throttles[k%n] = 0.15;
+	for(i=0; i<1000; i++) {
+	    motorImplementationSendThrottles(motorPins, n, throttles);
+	    usleep(1000);
 	}
 	// stop
-	throttles[k%4] = 0;
-	for(i=0; i<100; i++) {
-	    motorImplementationSendThrottles(motorPins, N, throttles);
-	    usleep(10000);
+	printf("Stop.\n");
+	throttles[k%n] = 0;
+	for(i=0; i<1000; i++) {
+	    motorImplementationSendThrottles(motorPins, n, throttles);
+	    usleep(1000);
 	}
 	// inverse spin
-	throttles[k%4] = -0.1;
-	for(i=0; i<100; i++) {
-	    motorImplementationSendThrottles(motorPins, N, throttles);
-	    usleep(10000);
+	printf("Inverse spin of motor %d.\n", k%n);
+	throttles[k%n] = -0.15;
+	for(i=0; i<1000; i++) {
+	    motorImplementationSendThrottles(motorPins, n, throttles);
+	    usleep(1000);
 	}
 	// stop
-	throttles[k%4] = 0;
-	for(i=0; i<100; i++) {
-	    motorImplementationSendThrottles(motorPins, N, throttles);
-	    usleep(10000);
+	printf("Stop.\n");
+	throttles[k%n] = 0;
+	for(i=0; i<1000; i++) {
+	    motorImplementationSendThrottles(motorPins, n, throttles);
+	    usleep(1000);
 	}
     }
     
     // stop motors
-    for(i=0; i<N; i++) throttles[i] = 0;
-    motorImplementationSendThrottles(motorPins, N, throttles);
+    for(i=0; i<n; i++) throttles[i] = 0;
+    motorImplementationSendThrottles(motorPins, n, throttles);
+    usleep(1000);
 
+    // Send beeps
+    for(k=0;k<10;k++) {
+	printf("Beacon %d.\n", k%5+1);
+	// beacons are dshot commands 1 to 5.
+	dshotRepeatSendCommand(motorPins, n, k%5+1, 1, 1);
+	// Not sure how exactly it works. There has to be a pause after having sent beacon command.
+	// However, if I do not send anything for a long time, motors are disarmed. So wait a bit
+	// and then send command 0 for some time.
+	usleep(10000);
+	dshotRepeatSendCommand(motorPins, n, 0, 0, 300);
+    }
+    
     // finalize
-    motorImplementationFinalize(motorPins, N);
+    motorImplementationFinalize(motorPins, n);
     
     return(0);
 }
